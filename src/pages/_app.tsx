4a32ee95fbdc326abc5e9a8c93403c327645e8b7
carlosallexandre <carlosallexandre@pop-os.localdomain>
2021-10-13 09:29:06 -0300

    chore: add player

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,22 @@
 import { AppProps } from 'next/app'
-
 import '../styles/global.scss'
 
+import { Header } from '../components/Header';
+import { Player } from '../components/Player';
+
+import styles from '../styles/app.module.scss';
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className={styles.container}>
+      <main>
+        <Header />
+        <Component {...pageProps} />
+      </main>
+
+      <Player />
+    </div>
+  );
 }
 
 export default MyApp
